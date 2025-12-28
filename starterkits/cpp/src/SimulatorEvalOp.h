ba5f60bde0a94dea8fc6032583dc66e325e75d6b
Andrija44 <macekandrija@gmail.com>
2025-12-28 21:03:04 +0100

    Spoji main na branch

---
+++ starterkits/cpp/src/SimulatorEvalOp.h
@@ -0,0 +1,14 @@
+#ifndef SIMULATOR_EVAL_OP_H
+#define SIMULATOR_EVAL_OP_H
+#include <ECF/ECF.h>
+#include "runner.h"
+#include "heuristic.h"
+
+class SimulatorEvalOp : public EvaluateOp
+{
+public:
+    FitnessP evaluate(IndividualP individual);
+};
+typedef std::shared_ptr<SimulatorEvalOp> SimulatorEvalOpP;
+
+#endif // SIMULATOR_EVAL_OP_H
---
+++ starterkits/cpp/src/heuristic.h
@@ -0,0 +1,52 @@
+#ifndef HEURISTIC_H
+#define HEURISTIC_H
+
+#include "simulator.h"
+
+#include <vector>
+#include <string>
+#include <ECF/ECF.h>
+
+namespace Tree {
+    class Tree;
+}
+
+enum class MoveType {
+    NONE,
+    ARRIVAL_TO_BUFFER,
+    BUFFER_TO_BUFFER,
+    BUFFER_TO_HANDOVER
+};
+
+struct Move {
+    MoveType type = MoveType::NONE;
+    int from = -1;
+    int to = -1;
+};
+
+class AbstractHeuristic {
+public:
+    virtual ~AbstractHeuristic() = default;
+    virtual Move calculate_move(BufferSimulator& sim) = 0;
+    static std::vector<Move> possible_moves(BufferSimulator& sim);
+    static bool apply_move(BufferSimulator& sim, Move& m);
+};
+
+class CustomHeuristic : public AbstractHeuristic {
+public:
+    Move calculate_move(BufferSimulator& sim) override;
+};
+
+class PriorityHeuristic : public AbstractHeuristic {
+public:
+    PriorityHeuristic(Tree::Tree* tree, std::vector<std::string>& terminal_names);
+    Move calculate_move(BufferSimulator& sim) override;
+private:
+    Tree::Tree* priority_tree;
+    std::vector<std::string> terminals;
+
+    double evaluate_move(BufferSimulator& sim, Move& m);
+    std::vector<double> extract_features(World& w);
+};
+
+#endif
